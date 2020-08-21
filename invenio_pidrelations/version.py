f958a1e5e5c9de7476d6680a618d02efbfd23ae6
Guillaume Viger <fenekku@fenekku.com>
2020-08-21 14:20:39 -0500

    release: v1.0.0a7

--- invenio_pidrelations/version.py
+++ invenio_pidrelations/version.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 #
 # This file is part of Invenio.
-# Copyright (C) 2017-2019 CERN.
+# Copyright (C) 2017-2020 CERN.
+# Copyright (C) 2020 Northwestern University.
 #
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -12,6 +13,4 @@ This file is imported by ``invenio_pidrelations.__init__``,
 and parsed by ``setup.py``.
 """
 
-from __future__ import absolute_import, print_function
-
-__version__ = '1.0.0a6'
+__version__ = '1.0.0a7'
